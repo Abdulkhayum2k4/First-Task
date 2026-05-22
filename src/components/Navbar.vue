cc3975c4d478f74962d0a0bf4d7113e74037a90e
Jaswant <jaswantmaheshwaram@gmail.com>
2026-05-22 15:00:19 +0530

    save

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar navbar-expand-lg custom-navbar px-3">
+    
+    <!-- Logo -->
+    <div class="logo-box me-4">
+      F.O.
+    </div>
+
+    <!-- Nav Items -->
+    <div class="navbar-nav d-flex flex-row gap-4">
+      <a class="nav-link active-link" href="#">Reservations</a>
+      <a class="nav-link" href="#">Pre check ins</a>
+      <a class="nav-link" href="#">Approvals</a>
+      <a class="nav-link" href="#">Promotions</a>
+      <a class="nav-link" href="#">Reports</a>
+      <a class="nav-link" href="#">Uploads</a>
+      <a class="nav-link" href="#">Supporting docs</a>
+      <a class="nav-link" href="#">Logs</a>
+      <a class="nav-link" href="#">Settings</a>
+    </div>
+
+  </nav>
+</template>
