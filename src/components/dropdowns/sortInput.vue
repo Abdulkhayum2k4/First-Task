8c392aa4daf2bfeb7132a586d4cdb2f2b17fc7e5
Abdulkhayum2k4 <abdulkhayoom2004@gmail.com>
2026-05-25 15:58:18 +0530

    abdul

---
+++ src/components/dropdowns/sortInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <select v-model="selectedSort">
+    <option value="sort">Sort</option>
+    <option value="ascending">Ascending</option>
+    <option value="descending">Descending</option>
+  </select>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const selectedSort = ref("");
+</script>
