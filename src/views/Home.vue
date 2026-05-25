cd38e53af0bfaa5cbf8f0cf325986340223557b2
Jaswant <jaswantmaheshwaram@gmail.com>
2026-05-25 10:44:34 +0530

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
-  <Navbar />
-  <Document />
+  <div class="bgclr">
+
+    <Navbar />
+    <Document />
+  </div>
 </template>
 
 <script setup>
 import Navbar from "../components/Navbar.vue";
 import Document from "@/components/Document.vue";
 </script>
+
+<style></style>
