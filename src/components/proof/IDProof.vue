9a7f3a054906d1081d9881b064e94a6b684da0ef
Abdulkhayum2k4 <abdulkhayoom2004@gmail.com>
2026-05-26 12:01:12 +0530

    abdul

--- src/components/proof/IDProof.vue
+++ src/components/proof/IDProof.vue
@@ -1,27 +1,33 @@
 <template>
+  <ProofHeader />
   <div class="container-fluid">
-    <div class="row ">
+    <div class="row">
       <div class="col-2 sidebar p-0">
         <div class="pt-3">
-
-          <div v-for="item in menuItems":key="item.name" class="menu-item" :class="{ active: item.active }">
-            <div class="d-flex justify-content-between align-items-center w-100">
+          <div
+            v-for="item in menuItems"
+            :key="item.name"
+            class="menu-item"
+            :class="{ active: item.active }"
+          >
+            <div
+              class="d-flex justify-content-between align-items-center w-100"
+            >
               <div class="d-flex align-items-center gap-2">
                 <span>{{ item.name }}</span>
-                <span v-if="item.alert" >
-                    <img :src="warning" alt="">
+                <span v-if="item.alert">
+                  <img :src="warning" alt="" />
                 </span>
               </div>
 
               <span v-if="item.completed" class="text-dark">
-                <img :src="blacktick" alt="">
+                <img :src="blacktick" alt="" />
               </span>
               <span v-if="item.anywarning">
-                <img :src="blackwarning" alt="">
+                <img :src="blackwarning" alt="" />
               </span>
             </div>
           </div>
-
         </div>
       </div>
 
@@ -29,49 +35,49 @@
         <!-- Right  -->
         <ProofCards />
       </div>
-
     </div>
   </div>
 </template>
 
 <script setup>
-import warning from '../../assets/redwarning.png'
-import blackwarning from '../../assets/blackwarning.png' 
-import blacktick from '../../assets/blacktick.png'
-import ProofCards from './ProofCards.vue'
+import warning from "../../assets/redwarning.png";
+import blackwarning from "../../assets/blackwarning.png";
+import blacktick from "../../assets/blacktick.png";
+import ProofCards from "./ProofCards.vue";
+import ProofHeader from "./ProofHeader.vue";
 
 const menuItems = [
   {
-    name: 'ID Proof',
+    name: "ID Proof",
     active: true,
     alert: true,
-    completed: false
+    completed: false,
   },
   {
-    name: 'Reg Card',
-    completed: true
+    name: "Reg Card",
+    completed: true,
   },
   {
-    name: 'Folios',
-    completed: true
+    name: "Folios",
+    completed: true,
   },
   {
-    name: 'Invoices',
-    completed: true
+    name: "Invoices",
+    completed: true,
   },
   {
-    name: 'POS Checks',
-    anywarning: true
+    name: "POS Checks",
+    anywarning: true,
   },
   {
-    name: 'Payments & c...',
-    completed: true
+    name: "Payments & c...",
+    completed: true,
   },
   {
-    name: 'Other details',
-    completed: true
-  }
-]
+    name: "Other details",
+    completed: true,
+  },
+];
 </script>
 
 <style scoped>
