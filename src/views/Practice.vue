10b7c227763ffacc99270e3ff66438952aac239f
Abdulkhayum2k4 <abdulkhayoom2004@gmail.com>
2026-05-27 10:24:59 +0530

    abdul

--- src/views/Practice.vue
+++ src/views/Practice.vue
@@ -1,11 +1,1 @@
-<script setup>
-import { ref } from "vue";
-
-const showText = ref(false);
-</script>
-
-<template>
-  <button @click="showText = !showText">Toggle</button>
-
-  <p v-if="showText" >This text is visible now.</p>
-</template>
+<template>dasfgh</template>
